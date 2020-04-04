17faacae73698b70101b8fbc85da843cf62b46aa
chenpengcode <cpcoder@aliyun.com>
2020-04-04 21:28:12 +0800

    堆 + 快速选择

--- emphasis/215_findKthLargest.py
+++ emphasis/215_findKthLargest.py
@@ -1,10 +1,11 @@
+import heapq
 from random import randint
 from typing import List
 
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        return self.select(nums, 0, len(nums) - 1, len(nums) - k)
+        return self.build_heap(nums, k)
 
     def partition(self, nums, left, right, pivot_index):
         pivot = nums[pivot_index]
@@ -29,25 +30,13 @@ class Solution:
             return self.select(nums, pivot_index + 1, right, k)
         return nums[k]
 
-    def build_heap(self, nums):
-        for i in reversed(range(len(nums))):
-            self.max_heapify(nums, i, len(nums))
-
-    def max_heapify(self, heap, root, heap_len):
-        p = root
-
-        child_pos = p * 2 + 1
-        while child_pos < heap_len:
-            right_pos = child_pos + 1
-            if right_pos < heap_len and heap[child_pos] < heap[right_pos]:
-                child_pos = right_pos
-
-            if heap[p] < heap[child_pos]:
-                heap[p], heap[child_pos] = heap[child_pos], heap[p]
-                p = child_pos
-                child_pos = p * 2 + 1
-            else:
-                break
+    def build_heap(self, nums, k):
+        heap = []
+        for num in nums:
+            heapq.heappush(heap, num)
+            if len(heap) > k:
+                heapq.heappop(heap)
+        return heapq.heappop(heap)
 
 
 if __name__ == '__main__':
