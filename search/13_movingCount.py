ad1634d4a53d726a2858b8ed304829099da36b58
chenpengcode <cpcoder@aliyun.com>
2020-04-09 00:01:33 +0800

    BFS

--- search/13_movingCount.py
+++ search/13_movingCount.py
@@ -14,3 +14,16 @@ class Solution:
                 if ((i - 1, j) in vis or (i, j - 1) in vis) and digitsum(i) + digitsum(j) <= k:
                     vis.add((i, j))
         return len(vis)
+
+    def movingCount_2(self, m: int, n: int, k: int) -> int:
+        from queue import Queue
+        q = Queue()
+        q.put((0, 0))
+        s = set()
+        while not q.empty():
+            x, y = q.get()
+            if (x, y) not in s and 0 <= x < m and 0 <= y < n and digitsum(x) + digitsum(y) <= k:
+                s.add((x, y))
+                for nx, ny in [(x + 1, y), (x, y + 1)]:
+                    q.put((nx, ny))
+        return len(s)
