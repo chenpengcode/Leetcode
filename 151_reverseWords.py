ac4d247943d5555ca21434681bc6da0eb3278e36
chenpengcode <cpcoder@aliyun.com>
2020-04-10 21:33:17 +0800

    python api 双端队列

---
+++ 151_reverseWords.py
@@ -0,0 +1,37 @@
+import collections
+
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        s_list = s.strip()
+        s_list = s.split()
+        tmp = reversed(s_list)
+        ans = " ".join(tmp)
+        return ans
+
+    def reverseWords_2(self, s: str) -> str:
+        left = 0
+        right = len(s) - 1
+        while left <= right and s[left] == ' ':
+            left += 1
+        while right >= left and s[right] == ' ':
+            right -= 1
+
+        d, word = collections.deque(), []
+        while left <= right:
+            if s[left] == ' ' and word:
+                d.append(''.join(word))
+                word = []
+            elif s[left] != ' ':
+                word.append(s[left])
+            left += 1
+
+        d.appendleft(''.join(word))
+        return ' '.join(d)
+
+
+
+if __name__ == '__main__':
+    s = "a good   example "
+    test = Solution()
+    print(test.reverseWords_2(s))
