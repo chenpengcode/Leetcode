eb2bdc2f06007125c22093e3f7e2c1ee7402ceb6
chenpengcode <cpcoder@aliyun.com>
2020-04-20 20:33:24 +0800

    dfs+bfs

--- search/200_numIslands.py
+++ search/200_numIslands.py
@@ -1,3 +1,4 @@
+import collections
 from typing import List
 
 
@@ -21,5 +22,28 @@ class Solution:
                 if grid[r][c] == "1":
                     num_islands += 1
                     self.dfs(grid, r, c)
+
+        return num_islands
+
+    def numIslands_2(self, grid: List[List[str]]) -> int:
+        nr = len(grid)
+        if nr == 0:
+            return 0
+        nc = len(grid[0])
+
+        num_islands = 0
+        for r in range(nr):
+            for c in range(nc):
+                if grid[r][c] == "1":
+                    num_islands += 1
+                    grid[r][c] = "0"
+                    neighbors = collections.deque([(r, c)])
+                    while neighbors:
+                        row, col = neighbors.popleft()
+                        for x, y in [(row - 1, col), (row + 1, col), (row, col - 1),
+                                     (row, col + 1)]:
+                            if 0 <= x < nr and 0 <= y < nc and grid[x][y] == "1":
+                                neighbors.append((x, y))
+                                grid[x][y] = "0"
 
         return num_islands
