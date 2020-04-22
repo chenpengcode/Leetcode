b3bc9e98ebd29aa020af7882e52734f90a1b5e7b
chenpengcode <cpcoder@aliyun.com>
2020-04-22 22:40:57 +0800

    dfs+bfs

---
+++ tree/199_rightSideView.py
@@ -0,0 +1,52 @@
+# Definition for a binary tree node.
+from collections import deque
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def rightSideView(self, root):
+        rightmost_value_at_depth = dict()  # 深度为索引，存放节点的值
+        max_depth = -1
+
+        stack = [(root, 0)]
+        while stack:
+            node, depth = stack.pop()
+
+            if node is not None:
+                # 维护二叉树的最大深度
+                max_depth = max(max_depth, depth)
+
+                # 如果不存在对应深度的节点我们才插入
+                rightmost_value_at_depth.setdefault(depth, node.val)
+
+                stack.append((node.left, depth + 1))
+                stack.append((node.right, depth + 1))
+
+        return [rightmost_value_at_depth[depth] for depth in range(max_depth + 1)]
+
+    def rightSideView_2(self, root):
+        rightmost_value_at_depth = dict()  # 深度为索引，存放节点的值
+        max_depth = -1
+
+        queue = deque([(root, 0)])
+        while queue:
+            node, depth = queue.popleft()
+
+            if node is not None:
+                # 维护二叉树的最大深度
+                max_depth = max(max_depth, depth)
+
+                # 由于每一层最后一个访问到的节点才是我们要的答案，因此不断更新对应深度的信息即可
+                rightmost_value_at_depth[depth] = node.val
+
+                queue.append((node.left, depth + 1))
+                queue.append((node.right, depth + 1))
+
+        return [rightmost_value_at_depth[depth] for depth in range(max_depth + 1)]
