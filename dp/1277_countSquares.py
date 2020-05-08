a09f1c957794493d72df542cec768e4c2cccb45e
chenpengcode <cpcoder@aliyun.com>
2020-05-08 21:04:52 +0800

    dp

---
+++ dp/1277_countSquares.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        if not matrix or not matrix[0]:
+            return 0
+
+        rows, columns = len(matrix), len(matrix[0])
+        dp = [[0] * columns for _ in range(rows)]
+        ans = 0
+
+        for i in range(rows):
+            for j in range(columns):
+                if matrix[i][j] == 1:
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+
+                    else:
+                        dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
+
+                ans += dp[i][j]
+
+        return ans
