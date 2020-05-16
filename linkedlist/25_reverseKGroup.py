353516a312d5d3e9f26603666b564b3ad62759a1
chenpengcode <cpcoder@aliyun.com>
2020-05-16 14:54:35 +0800

    模拟链表反转

---
+++ linkedlist/25_reverseKGroup.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        hair = ListNode(0)
+        hair.next = head
+        pre = hair
+        while head:
+            tail = pre
+            for i in range(k):
+                tail = tail.next
+                if not tail:
+                    return hair.next
+            nex = tail.next
+            head, tail = self.reverse(head, tail)
+            pre.next = head
+            tail.next = nex
+            pre = tail
+            head = tail.next
+        return hair.next
+
+    def reverse(self, head: ListNode, tail: ListNode):
+        prev = tail.next
+        p = head
+        while prev != tail:
+            nex = p.next
+            p.next = prev
+            prev = p
+            p = nex
+        return tail, head
