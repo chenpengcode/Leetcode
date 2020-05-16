af521b769792c61b6df36a8ad151095637a61988
chenpengcode <cpcoder@aliyun.com>
2020-05-16 15:46:11 +0800

    找出链表新的头尾节点

---
+++ linkedlist/61_rotateRight.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        if not head or not head.next:
+            return head
+        p = head
+        n = 0
+        while p:
+            n += 1
+            p = p.next
+        k %= n
+        # k移动n的整数倍，相当于没有移动
+        if k == 0:
+            return head
+        pre = ListNode(0)
+        pre.next = head
+        slow = fast = head
+        for i in range(k - 1):
+            fast = fast.next
+        while fast and fast.next:
+            pre = pre.next
+            slow = slow.next
+            fast = fast.next
+        fast.next = head
+        pre.next = None
+        return slow
