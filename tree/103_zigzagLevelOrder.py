24af9b4193389e5c50e2dd67be1efb9a79406f22
chenpengcode <cpcoder@aliyun.com>
2020-05-22 14:18:54 +0800

    dfs+bfs

--- tree/103_zigzagLevelOrder.py
+++ tree/103_zigzagLevelOrder.py
@@ -32,3 +32,39 @@ class Solution:
         for level in levels:
             ans.append(list(level))
         return ans
+
+    def zigzag_level_order_bfs(self, root: TreeNode) -> List[List[int]]:
+        ret = []
+        level_list = deque()
+        if root is None:
+            return []
+        # start with the level 0 with a delimiter
+        node_queue = deque([root, None])
+        is_order_left = True
+
+        while len(node_queue) > 0:
+            curr_node = node_queue.popleft()
+
+            if curr_node:
+                if is_order_left:
+                    level_list.append(curr_node.val)
+                else:
+                    level_list.appendleft(curr_node.val)
+
+                if curr_node.left:
+                    node_queue.append(curr_node.left)
+                if curr_node.right:
+                    node_queue.append(curr_node.right)
+            else:
+                # we finish one level
+                ret.append(level_list)
+                # add a delimiter to mark the level
+                if len(node_queue) > 0:
+                    node_queue.append(None)
+
+                # prepare for the next level
+                level_list = deque()
+                is_order_left = not is_order_left
+
+        return ret
+
