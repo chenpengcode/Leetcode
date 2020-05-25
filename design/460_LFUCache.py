add15be019445860743ea384d496fb9eb0411ba9
chenpengcode <cpcoder@aliyun.com>
2020-05-25 18:04:18 +0800

    双散列表

--- design/460_LFUCache.py
+++ design/460_LFUCache.py
@@ -1,7 +1,7 @@
 import collections
 
 
-class Node:
+class LinkedNode:
     def __init__(self, key, val, pre=None, nex=None, freq=0):
         self.pre = pre
         self.nex = nex
@@ -9,19 +9,19 @@ class Node:
         self.val = val
         self.key = key
 
-    def insert(self, nex):
-        nex.pre = self
-        nex.nex = self.nex
-        self.nex.pre = nex
-        self.nex = nex
+    def insert(self, node):
+        node.pre = self
+        node.nex = self.nex
+        self.nex.pre = node
+        self.nex = node
 
 
 def create_linked_list():
-    head = Node(0, 0)
-    tail = Node(0, 0)
+    head = LinkedNode(0, 0)
+    tail = LinkedNode(0, 0)
     head.nex = tail
     tail.pre = head
-    return (head, tail)
+    return head, tail
 
 
 class LFUCache:
@@ -63,7 +63,7 @@ class LFUCache:
                 node = self.keyMap[key]
                 node.val = value
             else:
-                node = Node(key, value)
+                node = LinkedNode(key, value)
                 self.keyMap[key] = node
                 self.size += 1
             if self.size > self.capacity:
