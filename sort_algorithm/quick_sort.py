1a352a3ee4cddf12548b0ddf555e6979e09fab32
chenpengcode <cpcoder@aliyun.com>
2020-06-01 23:28:39 +0800

    快排

---
+++ sort_algorithm/quick_sort.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+def quick_sort(nums: List[int], left, right):
+    if left >= right:
+        return
+
+    mid = partition(nums, left, right)
+    quick_sort(nums, left, mid - 1)
+    quick_sort(nums, mid + 1, right)
+
+
+def partition(nums, left, right):
+    pivot = nums[right]
+    index = left - 1
+
+    for i in range(left, right):
+        if nums[i] < pivot:
+            index += 1
+            nums[i], nums[index] = nums[index], nums[i]
+    index += 1
+    nums[index], nums[right] = nums[right], nums[index]
+    return index
+
+
+if __name__ == '__main__':
+    nums = [3, 6, 1, 5, 2, 4]
+    left, right = 0, 5
+    quick_sort(nums, left, right)
+    print(nums)
