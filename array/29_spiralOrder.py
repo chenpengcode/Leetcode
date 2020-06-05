9ac6a1051a5ed80b828d99486a7c8311d61cd2b0
chenpengcode <cpcoder@aliyun.com>
2020-06-05 22:30:28 +0800

    模拟

--- array/29_spiralOrder.py
+++ array/29_spiralOrder.py
@@ -28,6 +28,26 @@ class Solution:
             col += direction[index][1]
         return ans
 
+    def spiralOrder_2(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix or not matrix[0]:
+            return list()
+
+        rows, columns = len(matrix), len(matrix[0])
+        order = list()
+        left, right, top, bottom = 0, columns - 1, 0, rows - 1
+        while left <= right and top <= bottom:
+            for column in range(left, right + 1):
+                order.append(matrix[top][column])
+            for row in range(top + 1, bottom + 1):
+                order.append(matrix[row][right])
+            if left < right and top < bottom:
+                for column in range(right - 1, left, -1):
+                    order.append(matrix[bottom][column])
+                for row in range(bottom, top, -1):
+                    order.append(matrix[row][left])
+            left, right, top, bottom = left + 1, right - 1, top + 1, bottom - 1
+        return order
+
 
 if __name__ == '__main__':
     matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
