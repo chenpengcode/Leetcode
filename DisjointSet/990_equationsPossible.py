f026433f7429eb0bf08daa86fa35fb64c9a83688
chenpengcode <cpcoder@aliyun.com>
2020-06-08 15:46:47 +0800

    并查集

---
+++ DisjointSet/990_equationsPossible.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+class Solution:
+    class UnionFind:
+        def __init__(self):
+            self.parent = list(range(26))
+
+        def find(self, index):
+            if index == self.parent[index]:
+                return index
+            self.parent[index] = self.find(self.parent[index])
+            return self.parent[index]
+
+        def union(self, index1, index2):
+            self.parent[self.find(index1)] = self.find(index2)
+
+    def equationsPossible(self, equations: List[str]) -> bool:
+        uf = Solution.UnionFind()
+        for st in equations:
+            if st[1] == "=":
+                index1 = ord(st[0]) - ord("a")
+                index2 = ord(st[3]) - ord("a")
+                uf.union(index1, index2)
+        for st in equations:
+            if st[1] == "!":
+                index1 = ord(st[0]) - ord("a")
+                index2 = ord(st[3]) - ord("a")
+                if uf.find(index1) == uf.find(index2):
+                    return False
+        return True
+
+
+if __name__ == '__main__':
+    equations = ["a==b", "b==c", "a==c"]
+    solution = Solution()
+    print(solution.equationsPossible(equations))
