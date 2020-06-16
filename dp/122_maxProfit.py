c5c0ebc245b70d70add95b5f9238d56a796e9d1a
chenpengcode <cpcoder@aliyun.com>
2020-06-16 23:54:30 +0800

    buy[i] = max(buy[i - 1], sell[i - 1] - prices[i]) sell[i] = max(sell[i - 1], buy[i - 1] + prices[i])

--- dp/122_maxProfit.py
+++ dp/122_maxProfit.py
@@ -6,13 +6,14 @@ class Solution:
         if not prices:
             return 0
 
-        dp = [[0] * 2 for _ in range(len(prices))]
-        dp[0][0] = 0
-        dp[0][1] = -prices[0]
-        for i in range(1, len(prices)):
-            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
-            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
-        return dp[len(prices) - 1][0]
+        n = len(prices)
+        sell = [0] * n
+        buy = [0] * n
+        buy[0] = -prices[0]
+        for i in range(1, n):
+            sell[i] = max(sell[i - 1], buy[i - 1] + prices[i])
+            buy[i] = max(buy[i - 1], sell[i - 1] - prices[i])
+        return sell[-1]
 
     def maxProfit_better(self, prices: List[int]) -> int:
         if not prices:
