647e3791bf8020d09ed8fe1806a867d33fc7ab81
chenpengcode <cpcoder@aliyun.com>
2020-06-20 23:43:58 +0800

    二分查找

--- search/binary_search.py
+++ search/binary_search.py
@@ -29,7 +29,23 @@ def binary_search_2(array: List[int], x: int, lo=0, hi=None):
     return lo
 
 
+def binary_search_3(array: List[int], x: int, lo=0, hi=None):
+    if lo < 0:
+        raise ValueError('lo must be non-negative')
+    if not hi:
+        hi = len(array) - 1
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        if array[mid] == x:
+            return mid
+        elif array[mid] > x:
+            hi = mid - 1
+        else:
+            lo = mid + 1
+    return lo
+
+
 if __name__ == '__main__':
     arr = [1, 3, 3, 3, 3, 5, 7, 9]
     x = 4
-    print(binary_search(arr, x))
+    print(binary_search_3(arr, x))
