9b9d28ff4b2c24d8fdbb418736c305ab365fe3b0
chenpengcode <cpcoder@aliyun.com>
2020-07-06 23:25:04 +0800

    不同路径

---
+++ dp/62_uniquePaths.py
@@ -0,0 +1,20 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1] * n] * m
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[m - 1][n - 1]
+
+    def uniquePaths_n(self, m: int, n: int) -> int:
+        cur = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                cur[j] += cur[j - 1]
+        return cur[-1]
+
+
+if __name__ == '__main__':
+    m, n = 3, 2
+    solution = Solution()
+    print(solution.uniquePaths_n(m, n))
