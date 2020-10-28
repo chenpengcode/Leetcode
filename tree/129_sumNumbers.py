9a25b740d9c3d6c1dbbc49a9d07966ca303eef98
chenpengcode <cpcoder@aliyun.com>
2020-10-29 01:03:24 +0800

    129. 求根到叶子节点数字之和

--- tree/129_sumNumbers.py
+++ tree/129_sumNumbers.py
@@ -1,4 +1,7 @@
 # Definition for a binary tree node.
+import collections
+
+
 class TreeNode:
     def __init__(self, x):
         self.val = x
@@ -22,3 +25,26 @@ class Solution:
             return dfs(root.left, value) + dfs(root.right, value)
 
         return dfs(root, 0)
+
+    def sumNumbers_bfs(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        q1, q2 = collections.deque(), collections.deque()
+        q1.append(root)
+        q2.append(root.val)
+
+        ans = 0
+        while q1:
+            node = q1.popleft()
+            num = q2.popleft()
+            if not node.left and not node.right:
+                ans += num
+            else:
+                if node.left:
+                    q1.append(node.left)
+                    q2.append(num * 10 + node.left.val)
+                if node.right:
+                    q1.append(node.right)
+                    q2.append(num * 10 + node.right.val)
+        return ans
