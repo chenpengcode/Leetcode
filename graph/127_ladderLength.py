ac817d854f42cf847da5f0b4f9f887613a82a65e
chenpengcode <cpcoder@aliyun.com>
2020-11-05 16:55:22 +0800

    127. 单词接龙

---
+++ graph/127_ladderLength.py
@@ -0,0 +1,61 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        def addWord(word: str):
+            if word not in wordId:
+                nonlocal nodeNum
+                wordId[word] = nodeNum
+                nodeNum += 1
+
+        def addEdge(word: str):
+            addWord(word)
+            id1 = wordId[word]
+            chars = list(word)
+            for i in range(len(chars)):
+                tmp = chars[i]
+                chars[i] = "*"
+                newWord = "".join(chars)
+                addWord(newWord)
+                id2 = wordId[newWord]
+                edge[id1].append(id2)
+                edge[id2].append(id1)
+                chars[i] = tmp
+
+        wordId = dict()
+        edge = collections.defaultdict(list)
+        nodeNum = 0
+
+        for word in wordList:
+            addEdge(word)
+
+        addEdge(beginWord)
+        if endWord not in wordId:
+            return 0
+
+        dis = [float("inf")] * nodeNum
+        beginId, endId = wordId[beginWord], wordId[endWord]
+        dis[beginId] = 0
+
+        que = collections.deque([beginId])
+        while que:
+            x = que.popleft()
+            if x == endId:
+                return int(dis[endId] // 2 + 1)
+            for it in edge[x]:
+                if dis[it] == float("inf"):
+                    dis[it] = dis[x] + 1
+                    que.append(it)
+
+        return 0
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    beginWord = "hit"
+    endWord = "cog"
+    wordList = ["hot", "dot", "dog", "lot", "log", "cog"]
+
+    print(solution.ladderLength(beginWord, endWord, wordList))
