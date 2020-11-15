0510c6d9485386bcdc7ccb5a1866eb70bf2611de
chenpengcode <cpcoder@aliyun.com>
2020-11-16 06:44:56 +0800

    406. 根据身高重建队列

---
+++ greed/406_reconstructQueue.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        people.sort(key=lambda x: (x[0], -x[1]))
+        n = len(people)
+        ans = [[] for _ in range(n)]
+        for person in people:
+            spaces = person[1] + 1
+            for i in range(n):
+                if not ans[i]:
+                    spaces -= 1
+                    if spaces == 0:
+                        ans[i] = person
+                        break
+        return ans
+
+    def reconstructQueue_2(self, people: List[List[int]]) -> List[List[int]]:
+        people.sort(key=lambda x: (-x[0], x[1]))
+        n = len(people)
+        ans = list()
+        for person in people:
+            ans[person[1]:person[1]] = [person]
+        return ans
+
+
+if __name__ == '__main__':
+    people = [[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]]
+    solution = Solution()
+    print(solution.reconstructQueue_2(people))
