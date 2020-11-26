4f92911896c8b92c51650413b998e0bb1edfa4c0
chenpengcode <cpcoder@aliyun.com>
2020-11-27 01:21:23 +0800

    二叉树的右视图

--- tree/199_rightSideView.py
+++ tree/199_rightSideView.py
@@ -1,4 +1,5 @@
 # Definition for a binary tree node.
+import collections
 from collections import deque
 from typing import List
 
@@ -11,6 +12,9 @@ class TreeNode:
 
 
 class Solution(object):
+    def __init__(self):
+        self.ans = []
+
     def rightSideView(self, root):
         rightmost_value_at_depth = dict()  # 深度为索引，存放节点的值
         max_depth = -1
@@ -50,3 +54,46 @@ class Solution(object):
                 queue.append((node.right, depth + 1))
 
         return [rightmost_value_at_depth[depth] for depth in range(max_depth + 1)]
+
+    def rightSideView_3(self, root):
+        if not root:
+            return []
+        ans = []
+        deq = collections.deque([root])
+        while deq:
+            size = len(deq)
+            for i in range(size):
+                node = deq.popleft()
+                if i == size - 1:
+                    ans.append(node.val)
+                if node.left:
+                    deq.append(node.left)
+                if node.right:
+                    deq.append(node.right)
+        return ans
+
+    def rightSideView_4(self, root):
+        def helper(root, level):
+            if not root:
+                return
+            if len(self.ans) == level:
+                self.ans.append(root.val)
+            helper(root.right, level + 1)
+            helper(root.left, level + 1)
+
+        helper(root, 0)
+        return self.ans
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    node1 = TreeNode(1)
+    node2 = TreeNode(2)
+    node3 = TreeNode(3)
+    node4 = TreeNode(4)
+    node5 = TreeNode(5)
+    node1.left = node2
+    node1.right = node3
+    node2.right = node5
+    node3.right = node4
+    print(solution.rightSideView_4(node1))
